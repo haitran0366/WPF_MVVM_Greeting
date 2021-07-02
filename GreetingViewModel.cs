5d8d659efbff48de5d992d596cf9f93a1b4b044c
haitran0366 <haitran_usa@yahoo.com>
2021-07-02 11:32:38 -0700

    Add project files.

---
+++ GreetingViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Greeting
+{
+    public class GreetingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string name ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private string firstName;
+        public string FirstName 
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                NotifyPropertyChanged("LastName");
+            }
+        }
+
+        private string greeting;
+        public string Greeting
+        {
+            get { return greeting; }
+            set
+            {
+                greeting = value;
+                NotifyPropertyChanged("Greeting");
+            }
+        }
+
+        public ICommand cmdClickMe { get; set; }
+
+        private bool CanExecute
+        {
+            get { return !string.IsNullOrEmpty(FirstName) & !string.IsNullOrEmpty(LastName); }
+        }
+
+        public GreetingViewModel()
+        {
+            cmdClickMe = new RelayCommand(SayGreeting, () => CanExecute);
+        }
+
+        private void SayGreeting()
+        {
+            Greeting = "Hello " + FirstName + " " + LastName;
+        }
+    }
+}
---
+++ RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Greeting
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action action):this(action,() => true)
+        {
+            _execute = action;
+        }
+        public RelayCommand(Action action,Func<bool> canExecute)
+        {
+            _execute = action;
+            _canExecute = canExecute;
+        }
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
